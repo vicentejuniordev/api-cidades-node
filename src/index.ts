a54df0701faeed3583944f15b9bf9bbeeee8d06e
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-09-12 10:24:48 -0300

    feat:add config code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { server } from "./server/server";
+import { server } from './server/server';
 
 server.listen(3333, ()=> {
-    console.log("Sever is running...")
+    console.log('Sever is running...');
 });
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const server = express();
 
+
 server.get('/', (_, res)=>{
-    return res.send('Welcome, DEVS')
+    return res.send('Welcome, DEVS');
 });
 
 export {
     server
-}
+};
