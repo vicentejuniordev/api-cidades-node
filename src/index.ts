7211c835a8e4e10541d875c768ab0cec9e8f2ef5
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-09-13 15:01:25 -0300

    create var enviroment

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from './server/server';
 
-server.listen(3333, ()=> {
-    console.log('Sever is running...');
+server.listen(process.env.PORT || 3333, ()=> {
+    console.log(`Sever is running in port ${process.env.PORT || 3333}`);
 });
