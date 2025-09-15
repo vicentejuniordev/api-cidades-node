becbafe6722aa2fea2652115a3c8be17e4e8045e
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-09-15 08:49:11 -0300

    feat: add yup and validation body

--- src/controllers/cidades/create-city.ts
+++ src/controllers/cidades/create-city.ts
@@ -1,17 +1,35 @@
 import { Request, Response } from "express";
-
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface Icidade {
   nome: string
 }
 
+const bodyValidation: yup.ObjectSchema<Icidade> = yup.object().shape({
+  nome: yup.string().required().min(3)
+});
 
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export const create =(req: Request<{},{},Icidade>, res: Response)=>{
-  const data: Icidade = req.body;
-  console.log(data);
-  return res.send('Create!');
+export const create =async (req: Request<{},{},Icidade>, res: Response)=>{
+ let validated: Icidade | undefined = undefined;
+ try{
+
+  validated = await bodyValidation.validate(req.body) as Icidade;
+ 
+}catch(error){
+ 
+  const yupError = error as yup.ValidationError;
+  return res.status(StatusCodes.BAD_REQUEST).json({erros:{
+    default: yupError.message
+  }});
+
+  
+ };
+
+ console.log(validated);
+ return res.status(StatusCodes.OK).send("Create");
 };
 
 export const test ={};
