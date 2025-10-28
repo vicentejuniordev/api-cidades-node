1ad438266c832caf5910ee0323c230b1ccceb64d
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-10-28 14:48:21 -0300

    update for deploy

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,7 +2,7 @@ import "./shared/services/yup-translation";
 import express from 'express';
 import { router } from './routes';
 const server = express();
-
+import 'dotenv/config';
 server.use(express.json());
 server.use(router);
 
