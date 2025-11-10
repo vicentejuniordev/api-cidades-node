9908b4ee942d349c2c66b33d787f28b531fb515a
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-11-10 13:17:36 -0300

    fix: resolving produtcion bug

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import KnexInstace from './server/database/knex';
 import { server } from './server/server';
 
-server.listen(process.env.PORT || 3333, ()=> {
+const startServer = () =>{
+  server.listen(process.env.PORT || 3333, ()=> {
     console.log(`Sever is running in port ${process.env.PORT || 3333}`);
 });
+};
+
+if(process.env.IS_LOCALHOST != 'true'){
+  KnexInstace.migrate.latest().then(()=>{
+    startServer();
+  }).catch(console.log);
+}else{
+  startServer();
+};
